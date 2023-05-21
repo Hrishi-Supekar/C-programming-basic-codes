e0663074c3035b54a99b0624a992f423671dc66f
Hrishikesh Supekar <116080509+Hrishi-Supekar@users.noreply.github.com>
2023-05-21 22:15:49 +0530

    Update Q10_count_of_num.c

--- AssignmentDay4/Q10_count_of_num.c
+++ AssignmentDay4/Q10_count_of_num.c
@@ -1,25 +1,16 @@
 //10.Write a C program to count number of digits in a number.
 
 #include<stdio.h>
-//#include<string.h>
 void main()
 {
-//	int n;
-//	char x;
-//	printf("Enter the number:");
-//	scanf("%d",&n);
-//	
-//	x=char(n);
-//	printf("count of number:",x);
+int n,c=0;
+printf("Enter the number:");
+scanf("%d",&n);
 
-	int n,c=0;
-	printf("Enter the number:");
-	scanf("%d",&n);
-	
-	while(n>0)
-	{
-		n=n/10;
-		c++;
-	}
-	printf("The count of digits is :%d",c);
+while(n>0)
+{
+	n=n/10;
+	c++;
+}
+printf("The count of digits is :%d",c);
 }
