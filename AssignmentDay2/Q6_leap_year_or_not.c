d4bd278d0292f7e52ef9dcfef6a8c56e116123aa
Hrishikesh Supekar <116080509+Hrishi-Supekar@users.noreply.github.com>
2023-05-21 20:06:46 +0530

    Update Q6_leap_year_or_not.c
    changes in codes comments

--- AssignmentDay2/Q6_leap_year_or_not.c
+++ AssignmentDay2/Q6_leap_year_or_not.c
@@ -1,41 +1,41 @@
 //Write a C program to check whether a year is leap year or not.
 
-//#include<stdio.h>
-//int main()
-//{
-//	int year;
-//	printf("Enter the year to check:");
-//	scanf("%d",&year);
-//	
-//	if(year % 400 == 0)
-//	{
-//		printf("The year %d is a leap year",year);
-//	}
-//	else if(year % 100==0 && year %400 != 0)
-//	{
-//		printf("The year %d is not a leap year",year);
-//	}
-//	else if(year % 4 == 0)
-//	{
-//		printf("The year %d is leap year",year);
-//	}
-//	else
-//	{
-//		printf("The year %d is not a leap year",year);
-//	}
-//	return 0;
-//}
-
 #include<stdio.h>
 int main()
 {
 	int year;
-	printf("Enter the year:");
+	printf("Enter the year to check:");
 	scanf("%d",&year);
 	
-	if(year%4==0 && year%100!=0)
-	printf("The year is leap year!");
+	if(year % 400 == 0)
+	{
+		printf("The year %d is a leap year",year);
+	}
+	else if(year % 100==0 && year %400 != 0)
+	{
+		printf("The year %d is not a leap year",year);
+	}
+	else if(year % 4 == 0)
+	{
+		printf("The year %d is leap year",year);
+	}
 	else
-	printf("The year is not leap year!");
+	{
+		printf("The year %d is not a leap year",year);
+	}
 	return 0;
 }
+
+// #include<stdio.h>
+// int main()
+// {
+// 	int year;
+// 	printf("Enter the year:");
+// 	scanf("%d",&year);
+	
+// 	if(year%4==0 && year%100!=0)
+// 	printf("The year is leap year!");
+// 	else
+// 	printf("The year is not leap year!");
+// 	return 0;
+// }
