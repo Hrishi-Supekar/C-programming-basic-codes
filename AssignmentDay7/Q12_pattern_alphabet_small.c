acc3b70f1f2607d61dd7cd5c5f2c39f0eb3fef96
Hrishikesh Supekar <116080509+Hrishi-Supekar@users.noreply.github.com>
2023-05-22 08:37:43 +0530

    Update Q12_pattern_alphabet_small.c

--- AssignmentDay7/Q12_pattern_alphabet_small.c
+++ AssignmentDay7/Q12_pattern_alphabet_small.c
@@ -7,13 +7,12 @@
 #include<stdio.h>
 int main()
 {
-	int i,j,n,x;
+	int i,j,n,x=97;
 	printf("Enter the number:");
 	scanf("%d",&n);
 	
 	for(i=1;i<=n;i++)
 	{	
-		x=97;
 		for(j=1;j<=i;j++)
 		{
 			printf("%c",x++);
