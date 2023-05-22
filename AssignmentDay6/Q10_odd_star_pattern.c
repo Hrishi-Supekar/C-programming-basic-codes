cf90adf7abe22249e9034c6a5b6b94608058bb95
Hrishikesh Supekar <116080509+Hrishi-Supekar@users.noreply.github.com>
2023-05-22 08:30:07 +0530

    Update Q10_odd_star_pattern.c

--- AssignmentDay6/Q10_odd_star_pattern.c
+++ AssignmentDay6/Q10_odd_star_pattern.c
@@ -4,25 +4,25 @@
 //*****
 //*******
 
-#include<stdio.h>
-int main()
-{
-	int i,j,n,x=1;
-	printf("Enter the number:");
-	scanf("%d",&n);
+// #include<stdio.h>
+// int main()
+// {
+// 	int i,j,n,x=1;
+// 	printf("Enter the number:");
+// 	scanf("%d",&n);
 	
-	for(i=0;i!=n;i++)
-	{
-		for(j=1;j<=x;j++)
-		{
-			if(x%2!=0)
-				printf("*");
-		}
-		printf("\n");
-		x=x+2;
-	}
-	return 0;
-}
+// 	for(i=0;i!=n;i++)
+// 	{
+// 		for(j=1;j<=x;j++)
+// 		{
+// 			if(x%2!=0)
+// 				printf("*");
+// 		}
+// 		printf("\n");
+// 		x=x+2;
+// 	}
+// 	return 0;
+// }
 
 //10)Write a C program to print following pattern
 //*
@@ -30,23 +30,23 @@ int main()
 //*****
 //*******
 
-//#include<stdio.h>
-//int main()
-//{
-//	int i,j,n;
-//	printf("Enter the number:");
-//	scanf("%d",&n);
-//	n=(2*n);
-//	
-//	for(i=1;i<=n;i=i+2)
-//	{
-//		for(j=1;j<=i;j++)
-//		{
-//				printf("*");
-//		}
-//		printf("\n");
-//	}
-//}
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("Enter the number:");
+	scanf("%d",&n);
+	n=(2*n);
+	
+	for(i=1;i<=n;i=i+2)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
 
 //10)Write a C program to print following pattern
 //*
